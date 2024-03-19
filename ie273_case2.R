27dc166bdcf92214b9e73d59d99323cc016f4a9e
unknown <ericjames.abarro@gmail.com>
2024-03-20 04:58:11 +0800

    initial R file

--- ie273_case2.R
+++ ie273_case2.R
@@ -1,1 +1,8 @@
 #IE273
+
+setwd('C:/Users/ERIC ABARRO/OneDrive/Documents/ERIC JAMES/ie273_das')
+
+df_churn <- read.csv('ChurnTrain.csv', stringsAsFactors = TRUE)
+df_churn$Area.Code <- as.factor(df_churn$Area.Code)
+#df_churn$Churn <- as.factor(df_churn$Churn)
+summary(df_churn)
